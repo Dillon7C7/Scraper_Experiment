ab9b207e3e7d25d83f5c0fe74f40dbd17db00e74
Dillon Dommer <dillond@egalaxymultimedia.com>
2019-05-14 15:54:57 -0400

    Massive code cleanup

--- sel_first.py
+++ sel_first.py
@@ -16,6 +16,7 @@ import json
 import re
 import logging
 import sqlite3
+import re
 
 #class Database(object):
 #
@@ -34,6 +35,7 @@ class NakedNewsScraper(object):
 		self.chrome_options.add_argument('--start-maximized')
 		self.chrome_options.add_argument('--incognito')
 		self.delay = 10 # seconds
+		self.wait = '' # WebDriverWait object
 		self.username = ''
 		self.password = ''
 		self.browser = webdriver.Chrome(chrome_options=self.chrome_options, executable_path=self.chromedriver_path)
@@ -70,16 +72,17 @@ class NakedNewsScraper(object):
 		self.password = f.decrypt(password).decode()
 		
 	def load_browser(self):
-		'''load the web browser'''
+		''' Load the web browser. '''
 
 		self.browser.get(self.url)
+		self.wait = WebDriverWait(self.browser, self.delay)
+
 		try:
-			wait = WebDriverWait(self.browser, self.delay)
-			wait.until(EC.presence_of_element_located((By.ID, 'mobile-header-nav')))
+			self.wait.until(EC.presence_of_element_located((By.ID, 'mobile-header-nav')))
 			self.mobile_header_nav = self.browser.find_element_by_id('mobile-header-nav')
 
 		except Exception as e:
-			logging.error(e)
+			logging.error(e, exc_info=True)
 
 	def login(self):
 		''' Login to the web browser. '''
@@ -93,8 +96,7 @@ class NakedNewsScraper(object):
 
 		# handle login popup
 		try:
-			wait = WebDriverWait(self.browser, self.delay)
-			wait.until(EC.presence_of_element_located((By.ID, 'login_prompt')))
+			self.wait.until(EC.presence_of_element_located((By.ID, 'login_prompt')))
 			self.login_prompt = self.browser.find_element_by_id('login_prompt')
 			self.login_prompt.find_element_by_id('modal_customer_username').send_keys(self.username)
 			self.login_prompt.find_element_by_id('modal_customer_password').send_keys(self.password)
@@ -102,198 +104,140 @@ class NakedNewsScraper(object):
 			self.login_button2.click()
 
 		except Exception as e:
-			logging.error(e)
+			logging.error(e, exc_info=True)
 
 	def switch_to_archives(self):
 		''' Switch to archives. '''
 
-#		attempts = 0
-#		while (attempts < 8):
+		try:
 
-		wait = WebDriverWait(self.browser, self.delay)
-		wait.until(EC.invisibility_of_element_located((By.CSS_SELECTOR, '#login_prompt > div.modal-backdrop.in')))
+			# this element receives the click before the 'ARCHIVES' link, so we wait until it is not visible.
+			self.wait.until(EC.invisibility_of_element_located((By.CSS_SELECTOR, '#login_prompt > div.modal-backdrop.in')))
 
-		# this element element recieves the click before the 'ARCHIVES' button
-		#invis_element = self.browser.find_element_by_css_selector('#login_prompt > div.modal-backdrop.in')
+			#invis_element = self.browser.find_element_by_css_selector('#login_prompt > div.modal-backdrop.in')
 
-#			while invis_element.is_enabled():
-#				print('hohohoh')
+			# a child div seems to contain the links we need, but doesn't seem to work when clicking
+			self.wait.until(EC.presence_of_element_located((By.ID, 'header-nav')))
+			self.header_nav = self.browser.find_element_by_id('header-nav')
 
-#		except StaleElementReferenceException as se:
-#			logging.error(se)
+			# make sure link is ready to be clicked; all of these checks is likely overkill
+			self.wait.until(EC.presence_of_element_located((By.LINK_TEXT, 'ARCHIVES')))
+			self.wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'ARCHIVES')))
+			self.wait.until(EC.visibility_of_element_located((By.LINK_TEXT, 'ARCHIVES')))
 
-			#wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'modal-backdrop in')))
-##		try:
-		wait.until(EC.presence_of_element_located((By.ID, 'header-nav')))
-##
-		wait.until(EC.presence_of_element_located((By.LINK_TEXT, 'ARCHIVES')))
-		wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'ARCHIVES')))
-		wait.until(EC.visibility_of_element_located((By.LINK_TEXT, 'ARCHIVES')))
-##
-		self.header_nav = self.browser.find_element_by_id('header-nav')
-		self.header_nav.find_element_by_link_text('ARCHIVES').click()
-		######### working ###############
-			#da = self.top_button_nav.find_elements_by_css_selector('*')
-			#print(self.top_button_nav.find_elements_by_css_selector('*'))
-##
-##			#self.top_button_nav.find_elements_by_tag_name('li')[4].find_element_by_href().click()
-##
-##			# 4th button in the nav
-###			self.archive_button = self.top_button_nav.find_element_by_link_text('ARCHIVES')
-###			self.archive_button.click()
-##
-##		except Exception as e:
-##			print(e)
-##			logging.error(e)
+			# click on archives
+			self.header_nav.find_element_by_link_text('ARCHIVES').click()
 
-#		try:
-#			time.sleep(6)
-#			self.archive_button.click()
-#
-#		except StaleElementReferenceException as se:
-#			print('penis\;')
-	#		print(se)
-	#		wait.until(EC.presence_of_element_located((By.LINK_TEXT, 'ARCHIVES')))
-	#		self.archive_button = self.top_button_nav.find_element_by_css_selector('ul > li:nth-child(4) > a')
-	#		self.archive_button.click()
+		except StaleElementReferenceException as se:
+			logging.error(se, exc_info=True)
 
-	#		logging.error(se)
-
-	#	except Exception as e:
-	#		print(e)
-	#		logging.error(e)
-					
-
-			#attempts += 1
+		except Element as e:
+			logging.error(e, exc_info=True)
 	
 	def switch_to_segment_list(self):
 		''' Switch to segment list. '''	
 
-		attempts = 0
-		while (attempts < 8):
-			try:
-				wait = WebDriverWait(self.browser, self.delay)
-				wait.until(EC.presence_of_element_located((By.ID, 'subnav')))
+		try:
+			self.wait.until(EC.presence_of_element_located((By.ID, 'subnav')))
 
-				self.subnav = self.browser.find_element_by_ID('subnav')
-				self.search_by_segment_btn = self.subnav.find_element_by_css_selector('#subnav > div.container > ul > li:nth-child(2) > button')
+			self.subnav = self.browser.find_element_by_id('subnav')
+			self.search_by_segment_btn = self.subnav.find_element_by_css_selector('#subnav > div.container > ul > li:nth-child(2) > button')
 
-				self.search_by_segment_btn.click()
-				break
-					
-			except Exception as e:
-				logging.error(e)
-
-			attempts += 1
+			self.search_by_segment_btn.click()
+				
+		except Exception as e:
+			logging.error(e, exc_info=True)
 
 	def get_segment_types(self):
 		''' Get the segment types and store result in list self.segment_types and json. '''
 
-		attempts = 0
-		while (attempts < 8):
-			try:
-				wait = WebDriverWait(self.browser, self.delay)
+		try:
+			self.wait.until(EC.presence_of_element_located((By.ID, 'filter-segments')))
 
-				# web element containing list of segment types
-				wait.until(EC.presence_of_element_located((By.ID, 'filter-segments')))
-				self.segment_filter = self.browser.find_element_by_id('filter-segments')
-				
-				# div containing lsits of segment types
-				self.segment_filter_div = self.segment_filter.find_element_by_css_selector('#filter-segments > div > div')
+			# web element containing list of segment types
+			self.segment_filter = self.browser.find_element_by_id('filter-segments')
+			
+			# child div of child div containing list of segment types
+			self.segment_filter_div = self.segment_filter.find_element_by_css_selector('#filter-segments > div > div')
 
-				# list of segment types
-				self.segment_type_li = segment_filter_div.find_elements_by_tag_name('li')
-				self.segment_types = self.segment_type_li 
+			# list of selenium web objects containing segment type text
+			self.segment_type_li = self.segment_filter_div.find_elements_by_tag_name('li')
 
-				break
+			# list of segment type text
+			self.segment_types = [li.text for li in self.segment_type_li]
 
-			except Exception as e:
-				logging.error(e)
+			# create seperate class to make a database
+			with open('segment_types.json', 'w') as segment_json:
+				json.dump(self.segment_types, segment_json)
 
-			attempts += 1
+			#db = sqlite3.connect('MAY14.db')
+			#cursor = db.cursor()
+			#cursor.execute('''CREATE TABLE IF NOT EXISTS segment_types(type_id INTEGER PRIMARY KEY, segment_type TEXT NOT NULL UNIQUE''')
+			#db.commit()
+
+		except Exception as e:
+			logging.error(e, exc_info=True)
 
 	def get_segment_info(self, segment_ID):
 		''' Scrape segment info '''
 
-		#########################TEST STUFF#####################################
-		data = []
-		self.segment_type_li.find_element_by_link_text(segment_ID).click()
-	
-	#	try:
-	#		
-	#		wait.until(EC.presence_of_element_located((By.ID, 'arhive_index_view')))
-	#		archive_index_view = self.browser.find_element_by_id('arhive_index_view')
-	#		#wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'pagnation-controls')))
+		page_data = []
 
-	#		# used for conditional
-	#		segment_page_num_divs = self.browser.find_elements_by_class_name('pagnation-controls')
+		# link to a certain segment's archives page
+		self.wait.until(EC.element_to_be_clickable((By.LINK_TEXT, segment_ID)))
+		self.segment_filter_div.find_element_by_link_text(segment_ID).click()
 
-	#		if segment_page_num_divs:
-	#			
-	#			segment_page_num_div = self.browser.find_element_by_class_name('pagnation-controls')
-	#			next_button = segment_page_num_div.find_element_by_partial_link_text('NEXT')
+		#sdlfjasdklfsdfsdf# while the div containing the 'LAST' button exists, click
 
-	#			last_button = segment_page_num_div.find_element_by_partial_link_text('LAST')
-	#			num_pages = last_button.get_attribute('href')
-	#			last_page_num = int(num_pages[-1])
-	#		else:
-	#			last_page_num = 1
-	#			
-	#		video_page_div = archive_index_view.find_element_by_class_name('archives-content')
-	#		video_page_list = video_page_div.find_elements_by_css_selector('.archives-content > div > div > div')
-	#		#video_page_list = video_page_div.find_elements_by_css_selector('.archives-content > div > div > div > a')
+		while True:
 
-	#		for vid in video_page_list:
-	#			data.append(vid.text)
+			try:
+				# parent div containing nested divs which desired text
+				self.wait.until(EC.presence_of_element_located((By.ID, 'arhive_index_view')))
+				
+				# wait for first child div of arhive_index_view (container)
+				self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#arhive_index_view > div')))
+		
+				# wait for second child div of arhive_index_view (row-r)
+				self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#arhive_index_view > div > div')))
+				self.arhive_index_view_rows_container = self.browser.find_element_by_css_selector('#arhive_index_view > div > div')
+		
+				# list of all divs containing desired segment info on page
+				self.individual_segment_list = self.arhive_index_view_rows_container.find_elements_by_css_selector('#arhive_index_view > div > div > div')
+		
+				# loop through each div containing segment info
+				for segment in self.individual_segment_list:
+		
+					# Anchors + Segment Type
+					self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#arhive_index_view > div > div > div > div > div > a')))
+					anchor_and_segment = segment.find_element_by_css_selector('#arhive_index_view > div > div > div > div > div > a')
+		
+					# Date
+					self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#arhive_index_view > div > div > div > div > div > small')))
+					air_date = segment.find_element_by_css_selector('#arhive_index_view > div > div > div > div > div > small')
+		
+					# append anchors, segment type, and date to our list
+					page_data.append((air_date.text, anchor_and_segment.text))
 
-	#		for page in range(1, last_page_num):
+				# list containing web elements with the link text 'LAST »'
+				# **will break the program if it appears elsewhere on the page!
+				self.last_button_list = self.browser.find_elements_by_link_text('LAST »')
 
-	#			# click NEXT for every page
-	#			next_button.click()
-	#			wait.until(EC.presence_of_element_located((By.ID, 'arhive_index_view')))
-	#			archive_index_view = self.browser.find_element_by_id('arhive_index_view')
-	#			wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'pagnation-controls')))
-	#			segment_page_num_div = self.browser.find_element_by_class_name('pagnation-controls')
-	#			# if we are on the last page, do not look for the 'NEXT' button
-	#			if (page != last_page_num - 1):
-	#				next_button = segment_page_num_div.find_element_by_partial_link_text('NEXT')
-	#			video_page_div = archive_index_view.find_element_by_class_name('archives-content')
-	#			video_page_list = video_page_div.find_elements_by_css_selector('.archives-content > div > div > div')
+				# if the last_button_list is empty, then there is no "LAST" button on the page, and we can break
+				if not self.last_button_list:
+					break
+				
+				# else:
+				# loop through pages until no "LAST" link is found
+				self.page_list = self.browser.find_element_by_class_name('pagnation-controls')
+				self.next_button = self.page_list.find_element_by_link_text('NEXT ›')
+				self.next_button.click()
+		
+			# most important error check.
+			except Exception as e:
+				logging.error(e, exc_info=True)
 
-	#			for vid in video_page_list:
-	#				data.append(vid.text)
-	#				#print(vid.text)
-	#			
-
-	#	# this segment doesn't have a "last" button to worry about.
-	#	except TimeoutException:
-	#		print('PASS THIS SEGMENT. JUST ONE PAGE')		
-	#		print('JUST DO THE GRABBIN FROM FIRST PAGE!')
-	#	except Exception as e:
-	#		print('something else failed')
-	#		print(e)
-
-	#	filename = segment_ID + '_data.json'
-	#	rel_path = 'WEB_DATA/' + filename
-
-	#	with open(rel_path, 'w') as travel_file:
-	#		json.dump(data, travel_file)
-
-
-
-		#########################TEST STUFF#####################################
-
-		#try:
-		#	wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'pagnation-controls')))
-		#except Exception as e:
-		#	print(e)
-		#	print('xiaogou')
-
-
-		#last_but = self.browser.find_element_by_css_selector('#application > div.content > div.pagnation-controls.text-center > ul > li:nth-child(8) > a')
-
-		#for li in segment_type_li:
-		#	self.segment_types.append(li.text)
+		#print(page_data)
 
 	def scrape_all(self):
 
@@ -306,7 +250,6 @@ class NakedNewsScraper(object):
 			self.get_segment_type(seggy)
 			
 		
-		
 				
 
 ########################################
@@ -316,10 +259,10 @@ if __name__ == '__main__':
 	scraper.load_browser()
 	scraper.login()
 	scraper.switch_to_archives()
-	#scraper.switch_to_segment_list()
+	scraper.switch_to_segment_list()
+	scraper.get_segment_types()
+	scraper.get_segment_info('Travels')
 	#scraper.scrape_all()
-#	scraper.switch_to_segment_list()
-#	scraper.get_segment_type()
 #	scraper.switch_to_archives()
 	time.sleep(4)
 	scraper.browser.quit()
